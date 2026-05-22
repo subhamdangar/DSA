f219191f19f958ff28693e2e966fa6764208d1e5
SUBHAM DANGAR <dangarsubham@gmail.com>
2026-05-22 17:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.19%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,22 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range (len(nums)):
+#             for j in range (i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+        
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # test123
-        for i in range (len(nums)):
-            for j in range (i+1, len(nums)):
-                if nums[i] + nums[j] == target:
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
+
+        i = 0
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if (complement in d):
+                j = d[complement]
+                if i != j:
                     return [i,j]
-        
