b44f72a5ae0b88c13d2b952733e75d9213217758
SUBHAM DANGAR <dangarsubham@gmail.com>
2026-05-25 14:49:33 +0530

    Time: 15 ms (29.39%) | Memory: 20.8 MB (89.32%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,47 +1,67 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        pos = []
-        neg = []
-        n = len(nums)
-        for i in range(n):
-            if nums[i] >= 0:
-                pos.append(nums[i])
-            else:
-                neg.append(nums[i])
+# class Solution:
+#     def sortedSquares(self, nums: List[int]) -> List[int]:
+#         pos = []
+#         neg = []
+#         n = len(nums)
+#         for i in range(n):
+#             if nums[i] >= 0:
+#                 pos.append(nums[i])
+#             else:
+#                 neg.append(nums[i])
 
 
-        if (len(pos) == 0):
-            for i in range(n):
-                nums[i] = nums[i] ** 2
-            return nums[::-1]
-        elif (len(neg) == 0):
-            for i in range(n):
-                nums[i] = nums[i] ** 2
-            return nums
-        else:
-            # Merge two sorted array
-            pos = [x**2 for x in pos]
-            neg = [x**2 for x in neg]
-            neg = neg[::-1]
-            i = 0
-            j = 0
-            res = []
-            while(i<len(pos) and j <len(neg)):
-                if (pos[i] <= neg[j]):
-                    res.append(pos[i])
-                    i += 1
+#         if (len(pos) == 0):
+#             for i in range(n):
+#                 nums[i] = nums[i] ** 2
+#             return nums[::-1]
+#         elif (len(neg) == 0):
+#             for i in range(n):
+#                 nums[i] = nums[i] ** 2
+#             return nums
+#         else:
+#             # Merge two sorted array
+#             pos = [x**2 for x in pos]
+#             neg = [x**2 for x in neg]
+#             neg = neg[::-1]
+#             i = 0
+#             j = 0
+#             res = []
+#             while(i<len(pos) and j <len(neg)):
+#                 if (pos[i] <= neg[j]):
+#                     res.append(pos[i])
+#                     i += 1
 
-                else:
-                    res.append(neg[j])
-                    j += 1
+#                 else:
+#                     res.append(neg[j])
+#                     j += 1
 
-            while (j<len(neg)):
-                res.append(neg[j])
-                j += 1
-            while (i<len(pos)):
-                res.append(pos[i])
-                i += 1
-            return res
+#             while (j<len(neg)):
+#                 res.append(neg[j])
+#                 j += 1
+#             while (i<len(pos)):
+#                 res.append(pos[i])
+#                 i += 1
+#             return res
 
 
         
+
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        i = 0
+        j = n-1
+        res = [0]*n
+        while i<=j:
+            if (nums[i]**2 > nums[j]**2):
+                res[n-1] = nums[i]**2
+                n = n-1
+                i = i+1
+            else:
+                res[n-1] = nums[j]**2
+                n = n-1
+                j = j-1
+
+        return res
